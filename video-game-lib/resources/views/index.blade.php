c468d9847e3a765ee6ba67ab7f2b8b1b9ef7ec8d
vimis22 <vimis22@student.sdu.dk>
2023-11-08 20:40:07 +0100

    Shifted the files into different location, in terms of project structure.

--- video-game-lib/resources/views/index.blade.php
+++ video-game-lib/resources/views/index.blade.php
@@ -46,15 +46,33 @@
     </div>
 </nav>
 
+{{--<?php--}}
+{{--$gameboxtext = {"Test1","Test2","Test3","Test4","Test5"};--}}
+{{--?>--}}
+{{--@foreach ($gameboxtext as $text){--}}
+{{--<p>{{$text}}</p>--}}
+{{--}@endforeach--}}
+
 <section class="gamebox-placement content_placement">
+    //array(id,name,image.path,rating_scalar,user.name)
+    <?php
+    $gamebox_content = (
+    array(1, "image1","example.img",5,"bullshit"),
+    array(2, "image2","example.img",6,"bullshit"),
+    array(3, "image3","example.img",7,"bullshit"),
+    array(4, "image4","example.img",8,"bullshit"),
+    array(5, "image5","example.img",7,"bullshit"),
+    array(6, "image6","example.img",8,"bullshit")
+    );
+    ?>
+
+    @foreach($gamebox_content as $game){
+        <a href="gamepage.blade.php?gamename=<?=$game[1]?>" >
+        <img src="<?=$game[1]?>" >
+    @endforeach}
+
     <div class="gamebox content_placement">
-        <?php
-        $gameboxtext = {"Test1","Test2","Test3","Test4","Test5"};
-        ?>
-        @foreach ($gameboxtext as $text){
-            <p>{{$text}}</p>
-        }@endforeach
-        <a href="gamepage.html" class="gamebox_links">
+        <a href="gamepage.blade.php?gamename=slope" class="gamebox_links">
            <img src="EXAMPLE.png" class="game_image">
             <div class="game_name_placement content_placement">
              <p>
