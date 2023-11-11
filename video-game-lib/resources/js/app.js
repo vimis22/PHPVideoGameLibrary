bc49e033ef3e8cab5669012b484c9189249f6ea6
Casper <casper.fischer-gamer@hotmail.com>
2023-11-11 04:52:45 +0100

    Some start up work done
    Index loads. Javascript and stylingsheet moved to resources folder. Button "STRATEGY", "DRIVING&RACING", "ACTION, and "SHOOTING" now goes to their site when clicked.

--- video-game-lib/resources/js/app.js
+++ video-game-lib/resources/js/app.js
@@ -1,1 +1,33 @@
 import './bootstrap';
+
+function changePage(button) {
+    button.addEventListener("click", function() {
+        var url = button.dataset.route;
+        window.location.href = url;
+    });
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    var strategyButton = document.getElementById("strategyButton");
+    var drivingRacingButton = document.getElementById("drivingRacingButton");
+    var actionButton = document.getElementById("actionButton");
+    var shootingButton = document.getElementById("shootingButton");
+
+    if (strategyButton) {
+        changePage(strategyButton);
+    }
+
+    if (drivingRacingButton) {
+        changePage(drivingRacingButton);
+    }
+
+    if (actionButton) {
+        changePage(actionButton);
+    }
+
+    if (shootingButton) {
+        changePage(shootingButton);
+    }
+
+
+});
