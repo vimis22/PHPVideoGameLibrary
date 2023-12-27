25fd7aa3a24e10320cb4c27d5783dea5882cc0e2
Denisa <denisarissa@gmail.com>
2023-12-27 11:33:58 +0100

    minor css fix on show games

--- video-game-lib/resources/views/games/show.blade.php
+++ video-game-lib/resources/views/games/show.blade.php
@@ -1,10 +1,9 @@
 @extends('layouts.master')
-
-@section('content')
-    <body class="text-white">
-        <h1 class="text-white">{{ $game->name }}</h1>
-        <p class="text-white">{{ $game->description }}</p>
-        <p class="text-white">Publishing Date: {{ $game->publishing_date }}</p>
-        <p class="text-white">Category: {{ $game->category }}</p>
-    </body>
-@endsection
+@include('layouts.topbar')
+<hr class="text-white mx-3 my-4">
+<div class="text-center">
+    <h1 class="text-white">{{ $game->name }}</h1>
+    <p class="text-white">{{ $game->description }}</p>
+    <p class="text-white">Publishing date: {{ $game->publishing_date }}</p>
+    <p class="text-white">Category: {{ $game->category }}</p>
+</div>
