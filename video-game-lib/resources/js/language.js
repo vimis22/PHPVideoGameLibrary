581b318bfc6040f7c63f954a86cb3c02622abd5c
vimis22 <vimis22@student.sdu.dk>
2023-12-28 04:18:45 +0100

    Created a new branch, to make the initial changes and make viveks language extension. There have been problems in reviewing.

---
+++ video-game-lib/resources/js/language.js
@@ -0,0 +1,6 @@
+const languageselection = document.getElementById("languageselection");
+
+languageselection.addEventListener("selectionchange",function (){
+    const selected = languageselection.selected;
+    //Selected skal forbindes med PHP gennem en get eller post.
+});
